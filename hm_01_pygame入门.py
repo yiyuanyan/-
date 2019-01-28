7509df5acf4063373618194ca5b42505a055685b
何建新 <yiyuanhanly@126.com>
2019-01-29 00:03:24 +0800

    66666

---
+++ hm_01_pygame入门.py
@@ -0,0 +1,8 @@
+# 引入pygame框架
+import pygame
+# 必须首先初始化pygame
+pygame.init()
+
+print("游戏开始.....")
+# 结束代码必须退出pygame框架
+pygame.quit()
---
+++ hm_02_使用Rect描述英雄.py
@@ -0,0 +1,3 @@
+import pygame
+hero_rect = pygame.Rect(100, 500, 120, 125)
+print("英雄的原点：%d  %d 英雄的宽度：%d  %d" % (hero_rect.x, hero_rect.y, hero_rect.width, hero_rect.height))
